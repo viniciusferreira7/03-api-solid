875d04c14ce2bdb21192604bf537c814eda05d88
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-06-23 18:20:00 -0300

    style: eslint configuration

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
-import { app } from './app';
-import { env } from './env';
+import { app } from './app'
+import { env } from './env'
 
-app.listen({
-  host: '0.0.0.0',
-  port: env.PORT
-}).then((address) => console.log(`🚀 HTTP Server is Running ${address}`))
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then((address) => console.log(`🚀 HTTP Server is Running ${address}`))
