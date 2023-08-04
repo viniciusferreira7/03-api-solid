54fa51ceb18bd1fef0dde34292f3f59eed8764f3
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-08-04 20:44:32 -0300

    feat: Repository interface

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,4 +1,4 @@
-import { prisma } from '@/lib/prisma'
+import { UserRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
 
 interface RegisterUseCaseRequest {
@@ -9,16 +9,12 @@ interface RegisterUseCaseRequest {
 
 export class RegisterUseCase {
   // eslint-disable-next-line prettier/prettier
-  constructor(private userRepository: any) { }
+  constructor(private userRepository: UserRepository) { }
 
   async execute({ name, email, password }: RegisterUseCaseRequest) {
     const password_hash = await hash(password, 6)
 
-    const userWithSameEmail = await prisma.user.findUnique({
-      where: {
-        email,
-      },
-    })
+    const userWithSameEmail = await this.userRepository.findByEmail(email)
 
     if (userWithSameEmail) {
       throw new Error('Email already exists.')
