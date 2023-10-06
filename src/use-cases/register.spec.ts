5f26b021444e34a9256b14ba4e8b77abd43a5e90
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-10-06 17:28:45 -0300

    feat: First unit test

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,5 +1,21 @@
-import { expect, test } from 'vitest'
+import { PrismaUserRepository } from '@/repositories/prisma/prisma-users-repository'
+import { describe, expect, it } from 'vitest'
+import { RegisterUseCase } from './register'
+import { compare } from 'bcryptjs'
 
-test('check if it works', () => {
-  expect(1 + 2).toBe(3)
+describe('Register Use Case', () => {
+  it('should hash user password upon register', async () => {
+    const prismaUserRepository = new PrismaUserRepository()
+    const registerUseCase = new RegisterUseCase(prismaUserRepository)
+
+    const { user } = await registerUseCase.execute({
+      name: 'Vinicius',
+      email: 'vinicius@gmail.com',
+      password: '123',
+    })
+
+    const isPasswordCorrectlyHashed = await compare('123', user.password_hash)
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
 })
--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,3 +1,4 @@
+import { User } from '@prisma/client'
 import { PrismaUserRepository } from '@/repositories/prisma/prisma-users-repository'
 import { UsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
@@ -9,9 +10,17 @@ interface RegisterUseCaseRequest {
   password: string
 }
 
+interface RegisterUseCaseResponse {
+  user: User
+}
+
 export class RegisterUseCase {
   constructor(private usersRepository: UsersRepository) {}
-  async execute({ name, email, password }: RegisterUseCaseRequest) {
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
     const password_hash = await hash(password, 6)
 
     const userWithSameEmail = await new PrismaUserRepository().findByEmail(
@@ -22,10 +31,12 @@ export class RegisterUseCase {
       throw new UserAlreadyExistsError()
     }
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     })
+
+    return { user }
   }
 }
