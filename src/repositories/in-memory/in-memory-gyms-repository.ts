0b2a4ca31cc9974b0bb2ec4304e7506650ec6c0f
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-12-05 14:48:26 -0300

    feat: Use of nearby gyms

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,6 +1,7 @@
 import { Gym, Prisma } from '@prisma/client'
-import { GymsRepository } from '../gyms-repository'
+import { FetchNearbyParams, GymsRepository } from '../gyms-repository'
 import { randomUUID } from 'crypto'
+import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates'
 
 export class InMemoryGymsRepository implements GymsRepository {
   public items: Gym[] = []
@@ -13,6 +14,22 @@ export class InMemoryGymsRepository implements GymsRepository {
     }
 
     return gym
+  }
+
+  async findManyNearby(params: FetchNearbyParams) {
+    const gyms = this.items.filter((item) => {
+      const distance = getDistanceBetweenCoordinates(
+        { latitude: params.latitude, longitude: params.longitude },
+        {
+          latitude: item.latitude.toNumber(),
+          longitude: item.longitude.toNumber(),
+        },
+      )
+
+      return distance < 10
+    })
+
+    return gyms
   }
 
   async searchMany(query: string, page: number) {
