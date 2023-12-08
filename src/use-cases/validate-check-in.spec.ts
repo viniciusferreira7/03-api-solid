5556334947fd5e5c26580092422e08af23b585a1
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-12-08 17:37:48 -0300

    feat: Check-in validation use case

---
+++ src/use-cases/validate-check-in.spec.ts
@@ -0,0 +1,42 @@
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+import { ValidateCheckInUseCase } from './validate-check-in'
+import { ResourceNotFound } from './errors/resource-not-found-error'
+
+let checkInRepository: InMemoryCheckInsRepository
+let sut: ValidateCheckInUseCase
+
+describe('Validate Check-in Use Case', () => {
+  beforeEach(async () => {
+    checkInRepository = new InMemoryCheckInsRepository()
+    sut = new ValidateCheckInUseCase(checkInRepository)
+
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
+  it('should be able to validate check in', async () => {
+    const createdCheckIn = await checkInRepository.create({
+      user_id: 'userId-01',
+      gym_id: 'gymId-01',
+    })
+
+    const { checkIn } = await sut.execute({
+      checkInId: createdCheckIn.id,
+    })
+
+    expect(checkIn.validated_at).toEqual(expect.any(Date))
+    expect(checkInRepository.items[0].validated_at).toEqual(expect.any(Date))
+  })
+
+  it('should not be able to validate inexistent check in', async () => {
+    await expect(() =>
+      sut.execute({
+        checkInId: 'inexistent check in',
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFound)
+  })
+})
