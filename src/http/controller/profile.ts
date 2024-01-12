5bebe3c607f273f33fb237c1f887692797c78c0e
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-01-12 09:53:47 -0300

    feat: Implementing JWT in Fastify

--- src/http/controller/profile.ts
+++ src/http/controller/profile.ts
@@ -1,20 +1,10 @@
 import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
-import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
-import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-use-profile-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
-import { z } from 'zod'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  const profileBodySchema = z.object({
-    userId: z.string(),
-  })
-
-  const { userId } = profileBodySchema.parse(request.body)
-
-  const getUserProfileUseCase = makeGetUserProfileUseCase()
-
   try {
-    await getUserProfileUseCase.execute({ userId })
+    await request.jwtVerify()
+    console.log(request.user.sub)
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: err.message })
