9e7a000e1a722a6cb971ecbf59f90771cdda29bf
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-04-16 14:13:09 -0300

    feat: creates hash from from the password and validation

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,5 +1,6 @@
 import { prisma } from '@/lib/prisma'
 import { FastifyReply, FastifyRequest } from 'fastify'
+import { hash } from 'bcryptjs'
 import { z } from 'zod'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
@@ -11,8 +12,20 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body)
 
+  const password_hash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    return reply.status(409).send()
+  }
+
   const user = await prisma.user.create({
-    data: { name, email, password_hash: password },
+    data: { name, email, password_hash },
   })
 
   return reply.status(201).send()
