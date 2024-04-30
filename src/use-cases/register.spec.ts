a34dcd13ddc891fb226c7de76441f3b0fdeb86c3
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-04-30 15:56:56 -0300

    chore: config vitest

---
+++ src/use-cases/register.spec.ts
@@ -0,0 +1,11 @@
+import { describe, expect, it, test } from 'vitest'
+
+// describe('Register use case', () => {
+//   it('should be able to register user', async() => {
+
+//   })
+// })
+
+test('check if it works', () => {
+  expect(2 + 2).toBe(4)
+})
