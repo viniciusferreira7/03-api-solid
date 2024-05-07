d8fb76a6857699250d98d208739cff28056d8dd5
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-05-07 15:57:19 -0300

    feat: creates the authentication use case

---
+++ src/use-cases/authenticate.ts
@@ -0,0 +1,36 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { User } from '@prisma/client'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+import { compare } from 'bcryptjs'
+
+interface AuthenticateRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticateResponse {
+  user: User
+}
+
+export class AuthenticateUseCase {
+  constructor(private usersRepository: UsersRepository) { }
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateRequest): Promise<AuthenticateResponse> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doesPasswordMatches = await compare(password, user.password_hash)
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError()
+    }
+
+    return { user }
+  }
+}
