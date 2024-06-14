2b551c569b176d376da609eaf8aa6be727602ce2
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-06-14 08:29:46 -0300

    feat: creates profile controller

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,9 +1,29 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 
+import { ResourceNotFound } from '@/use-cases/errors/resource-not-found-error'
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
+
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
   await request.jwtVerify()
 
-  console.log(request.user.sub)
+  try {
+    const getUserProfile = makeGetUserProfileUseCase()
+
+    const { user } = await getUserProfile.execute({
+      userId: request.user.sub,
+    })
+
+    return reply.status(200).send({
+      user: {
+        ...user,
+        password_hash: undefined,
+      },
+    })
+  } catch (err) {
+    if (err instanceof ResourceNotFound) {
+      return reply.status(404).send({ message: err.message })
+    }
+  }
 
   return reply.status(200).send()
 }
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -3,6 +3,7 @@ import { FastifyInstance } from 'fastify'
 import { authenticate } from './controllers/authenticate'
 import { profile } from './controllers/profile'
 import { register } from './controllers/register'
+import { verifyJWT } from './middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
@@ -10,5 +11,5 @@ export async function appRoutes(app: FastifyInstance) {
   app.post('/sessions', authenticate)
 
   // Authenticated
-  app.get('/me', profile)
+  app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
