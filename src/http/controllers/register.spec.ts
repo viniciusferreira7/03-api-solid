d102051bf664e988d2dd2c12c0fe3e5b8ebfa2bc
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-06-28 08:10:21 -0300

    test: create a test e2e for registration

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,3 +1,23 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('register', () => { })
+import { app } from '@/app'
+
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    app.ready()
+  })
+
+  afterAll(async () => {
+    app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      password: '123456',
+    })
+    expect(response.statusCode).toEqual(201)
+  })
+})
