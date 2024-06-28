edb3be18414124ac2e9ca4f46cb709f35265e215
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-06-28 08:26:10 -0300

    test: creates a test e2e to authenticate

--- src/@types/fastify-jwt.d.ts
+++ src/@types/fastify-jwt.d.ts
@@ -2,7 +2,7 @@ import '@fastify/jwt'
 
 declare module '@fastify/jwt' {
   interface FastifyJWT {
-    payload: { id: number }
+    payload: null
     user: {
       sub: string
     }
--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,3 +1,31 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('authenticate', () => { })
+import { app } from '@/app'
+
+describe('Authenticate (e2e)', () => {
+  beforeAll(async () => {
+    app.ready()
+  })
+
+  afterAll(async () => {
+    app.close()
+  })
+
+  it('should be able to register', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      password: '123456',
+    })
+
+    const response = await request(app.server).post('/sessions').send({
+      email: 'john.doe@example.com',
+      password: '123456',
+    })
+
+    expect(response.body.token).toEqual(expect.any(String))
+
+    expect(response.statusCode).toEqual(200)
+  })
+})
