2016df7aa961b4fc93852c202031d049c359b79d
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-07-12 09:04:32 -0300

    feat: implements the refresh token

--- src/http/controllers/users/refresh.spec.ts
+++ src/http/controllers/users/refresh.spec.ts
@@ -3,7 +3,7 @@ import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
 import { app } from '@/app'
 
-describe('Authenticate (e2e)', () => {
+describe('Refresh Token (e2e)', () => {
   beforeAll(async () => {
     await app.ready()
   })
@@ -12,20 +12,32 @@ describe('Authenticate (e2e)', () => {
     await app.close()
   })
 
-  it('should be able to authenticate', async () => {
+  it('should be able to refresh a token', async () => {
     await request(app.server).post('/users').send({
       name: 'John Doe',
       email: 'john.doe@example.com',
       password: '123456',
     })
 
-    const response = await request(app.server).post('/sessions').send({
+    const authResponse = await request(app.server).post('/sessions').send({
       email: 'john.doe@example.com',
       password: '123456',
     })
 
+    const getCookies = authResponse.get('Set-Cookie')
+
+    const cookies = Array.isArray(getCookies) ? getCookies : []
+
+    const response = await request(app.server)
+      .patch('/token/refresh')
+      .set('Cookie', cookies)
+
     expect(response.statusCode).toEqual(200)
 
     expect(response.body.token).toEqual(expect.any(String))
+
+    expect(response.get('Set-Cookie')).toEqual([
+      expect.stringContaining('refreshToken='),
+    ])
   })
 })
